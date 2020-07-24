b5e9d7fb18f6f5e6e2ca46b9c04b124d7f0cd72e
Umar Arshad <umararshad1995@gmail.com>
2020-07-24 17:39:10 -0400

    updated readme

--- Develop/README.md
+++ Develop/README.md
@@ -1,2 +1,35 @@
-# README Generator
-
+  # README Generator
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Table of Contents
+  1. [Project Description](#description)
+  2. [Installation](#install)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributors](#contributors)
+  6. [Tests](#tests)
+  7. [Questions](#questions)
+  
+  ## Project Description
+  This is a README.md generator that will do all the formatting for you. All you have to do is answer the questions! Once you are done, thats it! README.md generated with all yur answered questions. Enjoy! :)
+  
+  ## Installation
+  Just install the inquirer (npm install) and you are good to go.
+  
+  ## Usage
+  After installing Inquirer, type in "node index" in the integrated terminal. Question prompts will appear for you to answer. Answer the questions and follow through to the end.
+  
+  ### License
+  MIT
+  
+  ### Contributors
+  Umar Arshad
+  
+  #### Tests
+  No tests.
+  
+  #### Questions (FAQs)
+  Q: Do I need to install anything? A: Yes, install Inquirer.
+  ###### Contact Me
+  * GitHub Profile: https://github.com/umar1425
+  * Email Address: umararshad1995@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,51 @@
 // function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
 
+  var template = `  # ${data.title}`;
+
+  if (data.license == "MIT") {
+    template += `\n  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  } else if (data.license == "Zlib") {
+    template += `\n [![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)](https://opensource.org/licenses/Zlib)`;
+  } else if (data.license == "Apache") {
+    template += `\n [![License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  };
+
+  return template + `
+  
+  ## Table of Contents
+  1. [Project Description](#description)
+  2. [Installation](#install)
+  3. [Usage](#usage)
+  4. [License](#license)
+  5. [Contributors](#contributors)
+  6. [Tests](#tests)
+  7. [Questions](#questions)
+  
+  ## Project Description
+  ${data.description}
+  
+  ## Installation
+  ${data.install}
+  
+  ## Usage
+  ${data.usage}
+  
+  ### License
+  ${data.license}
+  
+  ### Contributors
+  ${data.contributors}
+  
+  #### Tests
+  ${data.tests}
+  
+  #### Questions (FAQs)
+  ${data.questions}
+  ###### Contact Me
+  * GitHub Profile: https://github.com/${data.username}
+  * Email Address: ${data.email}
 `;
 }
 
